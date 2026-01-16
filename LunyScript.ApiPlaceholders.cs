45c6936e76a192d68386b011438fceba9721419b
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-01-16 11:06:04 +0100

    API outlining

---
+++ LunyScript.ApiPlaceholders.cs
@@ -0,0 +1,75 @@
+﻿namespace LunyScript
+{
+	public abstract partial class LunyScript
+	{
+		// these API outlines exist to get a feel for the intellisense/autocompletion behaviour ...
+
+		// planned API outline
+		public static class Animation {}
+		public static class Application {}
+		public static class Asset {}
+		public static class Camera {}
+		public static class Diagnostics {}
+		public static class HUD {}
+		public static class Input {}
+		public static class Menu {}
+		public static class Physics {}
+		public static class Player {}
+		public static class Prefab {}
+		public static class Storage {}
+
+		// possible future expansions
+		public static class Accessibility {}
+		public static class Account {}
+		public static class AI {}
+		public static class Async {}
+		public static class Avatar {}
+		public static class Cloud {}
+		public static class Cutscene {}
+		public static class Environment {}
+		public static class Graphics {}
+		public static class L18n {}
+		public static class Locale {}
+		public static class Localization {}
+		public static class Navigation {}
+		public static class Network {}
+		public static class NPC {}
+		public static class Particles {}
+		public static class Pool {}
+		public static class PostFx {}
+		public static class Progress {}
+		public static class Quality {}
+		public static class Script {}
+		public static class Session {}
+		public static class Settings {}
+		public static class Spawn {}
+		public static class Sprite {}
+		public static class Stage {}
+		public static class Terrain {}
+		public static class Tilemap {}
+		public static class Tutorial {}
+		public static class UI {}
+		public static class VFX {}
+		public static class Video {}
+
+		// publishing expansions
+		public static class Platform
+		{
+			public static class Desktop {}
+			public static class Linux {}
+			public static class Mobile {}
+			public static class OSX {}
+			public static class Web {}
+			public static class Windows {}
+			public static class XR {}
+		}
+
+		public static class Store
+		{
+			public static class Apple {}
+			public static class Epic {}
+			public static class Google {}
+			public static class Steam {}
+		}
+	}
+}
